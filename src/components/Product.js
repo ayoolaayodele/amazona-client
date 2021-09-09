c125449021ab44c8c2d5c1898765c2f950c4f05c
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-09 04:24:53 -0700

    Changed anchor tag to react-router-dom Link

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
-const Product = ({ product }) => {
+export default function Product({ product }) {
   return (
     <div key={product._id} className='card'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <img className='medium' src={product.image} alt={product.name} />
-      </a>
+      </Link>
       <div className='card-body'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
-        </a>
-        <Rating rating={product.rating} numReview={product.numReviews} />
+        </Link>
+        <Rating rating={product.rating} numReview={product.numReviews}></Rating>
         <div className='price'>${product.price}</div>
       </div>
     </div>
   );
-};
-
-export default Product;
+}
