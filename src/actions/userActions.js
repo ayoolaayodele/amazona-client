e8aa63bf22ce407af6bbe95fd22f6e79ffeef2be
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-11 10:02:39 -0700

    Modified file

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -10,7 +10,7 @@ import {
 } from "../constants/userConstants";
 
 export const register = (name, email, password) => async (dispatch) => {
-  dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
   try {
     const { data } = await Axios.post("/api/users/register", {
       name,
@@ -19,7 +19,6 @@ export const register = (name, email, password) => async (dispatch) => {
     });
     dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
     dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
-    localStorage.removeItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
