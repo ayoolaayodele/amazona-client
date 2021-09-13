8431abff232d32e91677588ac0a152b9b83f2d45
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-13 07:21:49 -0700

    Added a new action to save payment in the user

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS, } from "../constants/cartConstants";
 
 
 
@@ -33,4 +33,9 @@ export const removeFromCart = (productId) => (dispatch, getState) => {
 export const saveShippingAddress = (data) => (dispatch) => {
   dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
   localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
 };
