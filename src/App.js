6406faa7b23d5c42fffac8cd0ae9062fc4a34a41
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-13 09:15:54 -0700

    Added a route for place order

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from "./screens/SigninScreen";
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -61,6 +62,7 @@ function App() {
           <Route path='/register' component={RegisterScreen}></Route>
            <Route path="/shipping" component={ShippingAddressScreen}></Route>
            <Route path="/payment" component={PaymentMethodScreen}></Route>
+           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
         </main>
         <footer className='row center'>All right reserved</footer>
