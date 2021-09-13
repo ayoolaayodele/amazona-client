1fd1b98062e0c3276d0810415ee3034559c16638
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-13 05:22:14 -0700

    Added shipping address to  store

--- src/store.js
+++ src/store.js
@@ -21,7 +21,11 @@ const initialState = {
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
+         shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
   },
+  
 };
 const reducer = combineReducers({
   productList: productListReducer,
