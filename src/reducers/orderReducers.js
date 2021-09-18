e1bf47c0867b61fa395ea6a8ccba6ef33b03b267
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-18 08:49:10 -0700

    Created a new action, new constants and new reducer for delivery to check from the database if a product has been paid before rendering a delivery button

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -20,6 +20,10 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_RESET,
   ORDER_PAY_SUCCESS,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_RESET,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -112,3 +116,18 @@ export const orderDeleteReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return { loading: true };
+    case ORDER_DELIVER_SUCCESS:
+      return { loading: false, success: true };
+    case ORDER_DELIVER_FAIL:
+      return { loading: false, error: action.payload };
+    case ORDER_DELIVER_RESET:
+      return {};
+    default:
+      return state;
+  }
+}
