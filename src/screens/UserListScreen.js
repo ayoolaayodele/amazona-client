80f43042d9b92eca27879b54057a2728e891c417
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-20 06:16:56 -0700

    Dispatched user details reset to make it possible to go back and forth between userlist, userscreen  and user edit screen

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -3,17 +3,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, listUsers } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { USER_DETAILS_RESET } from '../constants/userConstants';
 
-export default function UserListScreen() {
+export default function UserListScreen(props) {
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
   const userDelete = useSelector((state) => state.userDelete);
-  const { error: errorDelete, success: successDelete } = userDelete;
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = userDelete;
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(listUsers());
+    dispatch({
+      type: USER_DETAILS_RESET,
+    });
   }, [dispatch, successDelete]);
   const deleteHandler = (user) => {
     if (window.confirm('Are you sure?')) {
@@ -23,6 +31,7 @@ export default function UserListScreen() {
   return (
     <div>
       <h1>Users</h1>
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {errorDelete && <MessageBox variant='danger'>{errorDelete}</MessageBox>}
       {successDelete && (
         <MessageBox variant='success'>User Deleted Successfully</MessageBox>
@@ -52,7 +61,12 @@ export default function UserListScreen() {
                 <td>{user.isSeller ? 'YES' : ' NO'}</td>
                 <td>{user.isAdmin ? 'YES' : 'NO'}</td>
                 <td>
-                  <button type='button' className='small'>
+                  <button
+                    type='button'
+                    className='small'
+                    onClick={() =>
+                      props.history.push(`/user/${user._id}/edit`)
+                    }>
                     Edit
                   </button>
                   <button
