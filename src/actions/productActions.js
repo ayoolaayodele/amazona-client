18aee66e4bd7f0203b5af9ea0a78968774726638
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-22 11:12:59 -0700

    We added name and pass it to the backend API as a query

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -18,14 +18,16 @@ import {
 } from '../constants/productConstants';
 
 export const listProducts =
-  ({ seller = '' }) =>
+  ({ seller = '', name = '' }) =>
   async (dispatch) => {
     //seller='' means the default value for seller is empty string but if we go to productlist screen
     dispatch({
       type: PRODUCT_LIST_REQUEST,
     });
     try {
-      const { data } = await Axios.get(`/api/products?seller=${seller}`);
+      const { data } = await Axios.get(
+        `/api/products?seller=${seller}&name=${name}`
+      );
       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     } catch (error) {
       dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
