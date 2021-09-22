035d77a5c6f6d3fdbd4fca07797dcd02a95592b0
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-22 09:13:28 -0700

    Added a new cart action that checks the cart item with the current product to add to cart, which checks if the seller of current product is different from the first item in the cart. A message is shown if the id of sellers is different

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import {
   CART_ADD_ITEM,
+  CART_ADD_ITEM_FAIL,
   CART_REMOVE_ITEM,
   CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
@@ -12,21 +13,34 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
 
   //send ajax request to server to get the information about any product
   const { data } = await Axios.get(`/api/products/${productId}`);
-  dispatch({
-    type: CART_ADD_ITEM,
-    payload: {
-      name: data.name,
-      image: data.image,
-      price: data.price,
-      countInStock: data.countInStock,
-      product: data._id,
-      seller: data.seller,
-      qty,
-    },
-  });
-  //store cart item in local localStorag
-  //implemented getState to have access to state in redux store
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+  const {
+    cart: { cartItems },
+  } = getState();
+  if (cartItems.length > 0 && data.seller._id !== cartItems[0].seller._id) {
+    dispatch({
+      type: CART_ADD_ITEM_FAIL,
+      payload: `Can't Add To Cart. Buy only from ${cartItems[0].seller.seller.name} in this order`,
+    });
+  } else {
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        product: data._id,
+        seller: data.seller,
+        qty,
+      },
+    });
+    //store cart item in local localStorag
+    //implemented getState to have access to state in redux store
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    );
+  }
 };
 
 export const removeFromCart = (productId) => (dispatch, getState) => {
