273f64681f0ccb88f5672bc5514e712b6f95b2ef
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-22 09:17:48 -0700

    Showed the error message in the cart screen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { addToCart, removeFromCart } from "../actions/cartActions";
-import MessageBox from "../components/MessageBox";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { addToCart, removeFromCart } from '../actions/cartActions';
+import MessageBox from '../components/MessageBox';
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
   const qty = props.location.search
-    ? Number(props.location.search.split("=")[1])
+    ? Number(props.location.search.split('=')[1])
     : 1;
   const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
+  const { cartItems, error } = cart;
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -25,13 +25,14 @@ export default function CartScreen(props) {
   };
 
   const checkoutHandler = () => {
-    props.history.push("/signin?redirect=shipping");
+    props.history.push('/signin?redirect=shipping');
   };
 
   return (
     <div className='row top'>
       <div className='col-2'>
         <h1>Shopping Cart</h1>
+        {error && <MessageBox variant='danger'>{error}</MessageBox>}
         {cartItems.length === 0 ? (
           <MessageBox>
             Cart is empty. <Link to='/'>Go Shopping</Link>
@@ -45,8 +46,7 @@ export default function CartScreen(props) {
                     <img
                       src={item.image}
                       alt={item.name}
-                      className='small'
-                    ></img>
+                      className='small'></img>
                   </div>
                   <div className='min-30'>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
@@ -58,8 +58,7 @@ export default function CartScreen(props) {
                         dispatch(
                           addToCart(item.product, Number(e.target.value))
                         )
-                      }
-                    >
+                      }>
                       {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
                           {x + 1}
@@ -71,8 +70,7 @@ export default function CartScreen(props) {
                   <div>
                     <button
                       type='button'
-                      onClick={() => removeFromCartHandler(item.product)}
-                    >
+                      onClick={() => removeFromCartHandler(item.product)}>
                       Delete
                     </button>
                   </div>
@@ -96,8 +94,7 @@ export default function CartScreen(props) {
                 type='button'
                 onClick={checkoutHandler}
                 className='primary block'
-                disabled={cartItems.length === 0}
-              >
+                disabled={cartItems.length === 0}>
                 Proceed to Checkout
               </button>
             </li>
