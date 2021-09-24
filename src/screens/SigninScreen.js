e7c426905d90eaa47055feb2ca5a765ddb6166ed
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-24 08:04:57 -0700

    Modified a sign in screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { signin } from "../actions/userActions";
-import LoadingBox from "../components/LoadingBox";
-import MessageBox from "../components/MessageBox";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function SigninScreen(props) {
-  const [email, setEmail] = useState("admin@example.com");
-  const [password, setPassword] = useState("123456");
+  const [email, setEmail] = useState('user@example.com');
+  const [password, setPassword] = useState('1234');
 
   const redirect = props.location.search
-    ? props.location.search.split("=")[1]
-    : "/";
+    ? props.location.search.split('=')[1]
+    : '/';
 
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo, loading, error } = userSignin;
@@ -42,8 +42,7 @@ export default function SigninScreen(props) {
             value={email}
             placeholder='Enter email'
             required
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
+            onChange={(e) => setEmail(e.target.value)}></input>
         </div>
         <div>
           <label htmlFor='password'>Password</label>
@@ -53,8 +52,7 @@ export default function SigninScreen(props) {
             value={password}
             placeholder='Enter password'
             required
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
+            onChange={(e) => setPassword(e.target.value)}></input>
         </div>
         <div>
           <label />
@@ -65,7 +63,7 @@ export default function SigninScreen(props) {
         <div>
           <label />
           <div>
-            New customer?{" "}
+            New customer?{' '}
             <Link to={`/register?redirect=${redirect}`}>
               Create your account
             </Link>
